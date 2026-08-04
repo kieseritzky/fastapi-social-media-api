a70ed16ea1762bf51ff32dfcf562bb71c56800c7
kieseritzky <asimmurtaza56@gmail.com>
2026-08-04 16:28:39 +0500

    Fix alembic migration

--- alembic/versions/92cf271fc0c5_add_user_table.py
+++ alembic/versions/92cf271fc0c5_add_user_table.py
@@ -24,7 +24,7 @@ def upgrade() -> None:
                     sa.Column('id', sa.Integer(), nullable=False),
                     sa.Column('email', sa.String(), nullable=False),
                     sa.Column('password', sa.String(), nullable=False),
-                    sa.Column('created_at', sa.TIMESTAMP(timezone=True)),
+                    sa.Column('created_at', sa.TIMESTAMP(timezone=True), nullable=False, server_default=sa.text('now()')),
                     sa.PrimaryKeyConstraint('id'),
                     sa.UniqueConstraint('email'))
     pass
